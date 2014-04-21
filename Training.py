3220a9d0cc0a7646522d28778663fa2591097355
Alex Lombardi <alexlombardi01@college.harvard.edu>
2014-04-20 22:12:45 -0400

    stuff

---
+++ Training.py
@@ -0,0 +1,12 @@
+class Training
+
+def textconversion(haikus.txt): #returns a list of haikus, given a large text file.
+
+def Train_haiku(haiku, monograms, bi-grams, line_types) 
+# Takes in a new Haiku as well as three sets: one of Monogram objects, one of Bi-Gram objects,
+and one of Line_type objects. Updates the Monogram set and the Bi-Gram set, returns unit.
+
+def Train (haikus, monograms, bi-grams, line_types) =
+         List.iter (x -> Train_haiku(x, monograms, bi-grams, line_types)) haikus
+         
+end
