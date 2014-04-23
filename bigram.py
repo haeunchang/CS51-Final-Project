85e4a64a62333dbf3e17065f6ac1803a803738bb
Sandra Schumann <sandra.schumann426@gmail.com>
2014-04-23 17:39:30 -0400

    bigram update + minor fix

--- bigram.py
+++ bigram.py
@@ -2,11 +2,15 @@ class Bi_Gram:
     """A bi-gram will store pair occurrences in the training data."""
     
     def __init__(self, word1, word2):
-        self.phrase = (string1, string2) # the word phrase
+        self.phrase = (word1, word2) # the word phrase
         self.occurrences = 0 # counts occurrences
     
     def update(self, haiku):
         """Increases self.occurrences based on data from haiku."""
-        raise NotImplementedError
+        for line in haiku.lines:
+            words = line.split()
+            for i in range(len(words)-1):
+                if (words[i],words[i+1]) == self.phrase:
+                    self.occurrences += 1
         
 
