753c95e1ca7d37d9c3b7d2ef1a9158763b4144ab
Sandra Schumann <sandra.schumann426@gmail.com>
2014-04-25 15:10:09 -0400

    put the inputting function in dictionary file

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,6 @@
 # This module contains functions for interacting with several external databases - dictionaries
 
+import haiku
 import curses
 from curses.ascii import isdigit
 import nltk
@@ -46,3 +47,23 @@ def is_word (w):
     """Returns a boolean describing whether or not this word falls into
     any of the four wordtypes defined above"""
     return (True in dictionary.wordtype(w))
+
+def read_input(textfile):
+    """Returns a list of Haiku objects"""
+    fin = open("testhaikus.txt", 'r')
+
+    t = []
+    for line in fin.read().split('\n'):
+        phrases = line.split()
+        words = []
+        for phrase in phrases:
+            (words.append(w.translate(string.maketrans("",""), string.punctuation)) for w in phrase.split('-') if w)
+        t.append(words)
+
+    fin.close()
+
+    haikus = []
+    for i in range(len(t)/4):
+        haikus.append(haiku.Haiku(t[i*4+1],t[i*4+2],t[i*4+3],raw_lines = True))
+    
+    return haikus
