023694f1f98c5300deed955ba13b1fafe44c6fe9
Sandra Schumann <sandra.schumann426@gmail.com>
2014-04-25 14:39:39 -0400

    dictionary and added testhaikus

--- dictionary.py
+++ dictionary.py
@@ -5,8 +5,11 @@ from curses.ascii import isdigit
 import nltk
 from nltk.corpus import cmudict
 from nltk.corpus import wordnet
- 
+from hyphen.dictools import 
+from hyphen import Hyphenator, dict_info
+
 d = cmudict.dict() 
+h = Hyphenator('en_US')
 
 def word_filter (word):
     """Removes possessive from the end of the word for classification purposes"""
@@ -15,19 +18,32 @@ def word_filter (word):
     else:
         return word
 
-
 def syllablecnt (word):
     """Returns the number of syllables in a word."""
-    return [len(list(y for y in x if isdigit(y[-1]))) for x in d[word.lower()]]
+    if word.lower() in d:
+        counts = [len(list(y for y in x if isdigit(y[-1]))) for x in d[word.lower()]]
+        if len(counts) > 0:
+            return counts[0]
+    return h.syllables(word)
 
 def wordtype (word):
     """Returns the lexical category of a word (part of speech)."""
-    raise NotImplementedError
-    
-def is_word (word)
+    wntypes = wordnet.synsets(word)
+    if len(wntypes) > 0:
+        t = [synset.pos() for synset in wntypes[:len(wntypes)/2+1]
+        types = [False, False, False, False, word]
+        if 'n' in t:
+            types[0] = True
+        if 'v' in t:
+            types[1] = True
+        if 's' in t:
+            types[2] = True
+        if 'r' in t:
+            types[3] = True
+        return types
+    return None
+
+def is_word (w):
     """Returns a boolean describing whether or not this word falls into
     any of the four wordtypes defined above"""
-    return (dictionary.wordtype(w)[0] != False or 
-    dictionary.wordtype(w)[1] != False or
-	dictionary.wordtype(w)[2] != False or
-	dictionary.wordtype(w)[3] != False)
+    return (True in dictionary.wordtype(w))
