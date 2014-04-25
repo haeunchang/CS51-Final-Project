93d23d1092d0421b15bddee1de9078121df00d95
spencerkwon <spencer.y.kwon@gmail.com>
2014-04-25 00:23:44 -0400

    hoa

---
+++ evolve_population.py
@@ -0,0 +1,58 @@
+# Needs a evolve object
+import Haiku_evo as evo_object
+import random
+import math
+
+class Evolve_population:
+
+    population_list = []
+    total_population = 0
+    mutation_parameter = 0
+    cross_pollination_parameter = 0
+    
+    # initializes population
+    def __init__(self, my_total_population, my_mutation_parameter, my_cross_pollination_parameter):
+        self.mutation_parameter = my_mutation_parameter
+        self.cross_pollination_paramter = my_cross_pollination_parameter
+        self.total_population = my_total_population
+        for x in range (0, my_total_population):
+            # Creates new object
+            new_object = evo_object()
+            # is this legit?
+            self.population_list.append(new_object)
+        # sorts the list
+        total_population.sort(evo_object.compare)
+    
+    # as of now, uniformly chooses elements to mutate
+    def create_mutants (base_population):
+        x = random.randint(0, len(base_population)- 1)
+        to_be_mutated = base_population[x]
+        mutated_object = evo_object.mutate()
+        return mutated_object
+
+    # similarly, uniformly chooses elements to cross_pollinate
+    # Note that if it chooses the same one, it will effectively replicate the said object, which is not bad
+    def create_cross_pollinated (base_population):
+        x = random.randint(0, len(base_population) - 1)
+        y = random.randint(0, len(base_population) - 1)
+        pollinated_object = evo_object.cross_pollinate (base_population[x], base_population[y])
+        return pollinated_object   
+        
+    def update_next_generation(self)
+        mutant_pop = math.floor(self.total_population * self.mutation_parameter)
+        cross_pop = math.floor(self.total_population * self.cross_pollintion_parameter)
+        prev_pop = self.population_list
+        # is this legit? how do i use methods within?      
+        for x in range (0, mutant_pop):
+            new_mutant = self.create_mutants(prev_pop)
+            self.population_list.append (new_mutant)
+        for x in range (0, cross_pop):
+            new_cross = self.create_cross_pollinated(prev_pop)
+            self.population_list.append (new_cross)    
+        
+        # sorts it
+        self.population.sort(evo_object.compare)
+        # cuts off the least successful ones
+        self.population_list = self.population_list [0: total_population]
+       
+
