2fce87f626eec0e105d0efb6ed6adbcb215f1b6f
spencerkwon <spencer.y.kwon@gmail.com>
2014-04-25 15:36:12 -0400

    evo_object.py and evolve_population.py

---
+++ evo_object.py
@@ -0,0 +1,62 @@
+from line_haiku import Line_Haiku
+import random
+
+class Evo_object:
+	def __init__(self, line1, line2, line3, raw_lines = False):
+        if raw_line:
+            line1 = Line_Haiku(line1, 1)
+            line2 = Line_Haiku(line2, 2)
+            line3 = Line_Haiku(line3, 3)
+            
+		self.triple = [line1, line2, line3]
+        self.score = 0
+    	    
+    def get_score (self):
+        return self.score
+
+    def get_triple (self):
+        return self.triple
+    
+    def mutate_score(self, evaluated_score):
+        self.score = evaluated_score
+
+## ANCILLARY FUNCTIONS
+    	
+def compare(x, y):
+    return(x.get_score() - y.get_score())
+
+def cross_pollinate (evo_object1, evo_object2):
+    new_triple = []
+    for y in range(0, 2):
+        if random.randint(0,1) == 0:
+            new_triple.append(evo_object1.get_triple[y])
+        else:
+            new_triple.append(evo_object2.get_triple[y])
+    new_kid = Evo_object(new_triple[0], new_triple[1], new_triple[2])
+    return new_kid
+    
+def mutate (evo_object1)
+    mutated_object = evo_object1
+    return mutated_object
+    
+def gen_random_evo():
+    raise NotImplementedError
+#    random_object = Evo_object(random_triple, score)
+#    return random_object
+    
+# Helper function that generates an element according to the probability in the array
+# There is already a thing implemented in numpy, but hell
+
+def random_weighted_occurrence (list_skeleton, list_weight):
+    # assumes list1 is positive
+    tot_sum = sum(list_weight)
+    x = random.uniform(0, tot_sum)
+    y = 0    
+    while (True):
+        if x < list_weight[y]:
+            return list_skeleton[y]
+        else:
+            x = x - list_weight[y]
+            y = y + 1
+
+    
--- evolve_population.py
+++ evolve_population.py
@@ -1,5 +1,5 @@
 # Needs a evolve object
-import Haiku_evo as evo_object
+import evo_object as evo_object
 import random
 import math
 
@@ -17,42 +17,39 @@ class Evolve_population:
         self.total_population = my_total_population
         for x in range (0, my_total_population):
             # Creates new object
-            new_object = evo_object()
-            # is this legit?
+            new_object = evo_object.gen_random_evo()
             self.population_list.append(new_object)
         # sorts the list
-        total_population.sort(evo_object.compare)
-    
-    # as of now, uniformly chooses elements to mutate
-    def create_mutants (base_population):
-        x = random.randint(0, len(base_population)- 1)
-        to_be_mutated = base_population[x]
-        mutated_object = evo_object.mutate()
-        return mutated_object
-
-    # similarly, uniformly chooses elements to cross_pollinate
-    # Note that if it chooses the same one, it will effectively replicate the said object, which is not bad
-    def create_cross_pollinated (base_population):
-        x = random.randint(0, len(base_population) - 1)
-        y = random.randint(0, len(base_population) - 1)
-        pollinated_object = evo_object.cross_pollinate (base_population[x], base_population[y])
-        return pollinated_object   
-        
+        self.population_list.sort(evo_object.compare)
+            
     def update_next_generation(self)
-        mutant_pop = math.floor(self.total_population * self.mutation_parameter)
-        cross_pop = math.floor(self.total_population * self.cross_pollintion_parameter)
+        mutant_pop = int(self.total_population * self.mutation_parameter)
+        cross_pop = int(self.total_population * self.cross_pollintion_parameter)
         prev_pop = self.population_list
-        # is this legit? how do i use methods within?      
+        # Creates random pollination/mutation      
         for x in range (0, mutant_pop):
-            new_mutant = self.create_mutants(prev_pop)
+            new_mutant = create_mutants(prev_pop)
             self.population_list.append (new_mutant)
         for x in range (0, cross_pop):
-            new_cross = self.create_cross_pollinated(prev_pop)
+            new_cross = create_cross_pollinated(prev_pop)
             self.population_list.append (new_cross)    
-        
         # sorts it
         self.population.sort(evo_object.compare)
         # cuts off the least successful ones
         self.population_list = self.population_list [0: total_population]
        
+# as of now, uniformly chooses elements to mutate
+def create_mutants (base_population):
+    x = random.randint(0, len(base_population)- 1)
+    to_be_mutated = base_population[x]
+    mutated_object = evo_object.mutate()
+    return mutated_object
+
+# similarly, uniformly chooses elements to cross_pollinate
+# Note that if it chooses the same one, it will effectively replicate the said object, which is not bad
+def create_cross_pollinated (base_population):
+    x = random.randint(0, len(base_population) - 1)
+    y = random.randint(0, len(base_population) - 1)
+    pollinated_object = evo_object.cross_pollinate (base_population[x], base_population[y])
+    return pollinated_object   
 
